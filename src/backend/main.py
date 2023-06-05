90e1b04d194fc7f59ee31530fbbde1cbd6f3b8ca
SergioZona <60414505+SergioZona@users.noreply.github.com>
2023-06-05 01:49:07 -0500

    Push

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,4 +1,5 @@
 """Aditional libraries"""
+import time
 import pandas as pd
 import numpy as np
 import warnings
@@ -7,15 +8,12 @@ from .recommendation_systems import model
 from surprise import dump
 
 """Backend dependencies"""
-import sqlite3
 import uvicorn
 from fastapi import FastAPI, Body
 from fastapi.middleware.cors import CORSMiddleware
-import hashlib
 from datetime import datetime
+from surprise import dump
 
-
-# Issue corrected: https://stackoverflow.com/questions/65635346/how-can-i-enable-cors-in-fastapi
 # Create an instance of the FastAPI class
 app = FastAPI()
 app.add_middleware(
@@ -26,99 +24,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Connect to the SQLite3 database
-# conn = sqlite3.connect('../data/data.db') # Si ejecuto el script desde SRC.
-conn = sqlite3.connect('data/data.db')
+# Get CSV
+df_movies = pd.read_csv('backend/data/df_movies.csv')
+df_movies.fillna("", inplace=True)
+
+df_reviews = pd.read_csv('backend/data/df_ratings.csv')
+df_reviews.fillna("", inplace=True)
+
+df_links = pd.read_csv('backend/data/df_links.csv')
+df_links.fillna("", inplace=True)
+
+knn = dump.load('backend/data/knn.pkl')[1]
 
 @app.get("/")
 def root():
     return {"message": "Fast API in Python"}
-
-"""----------------"""
-"""ENDPOINTS - USER"""
-"""----------------"""
-
-# Define a route to get all the rows from the database table
-
-@app.get('/api/user')
-async def get_user():
-    # Create a cursor object
-    cur = conn.cursor()
-
-    # Execute a SELECT statement to get all the rows from the table
-    cur.execute('SELECT * FROM user')
-
-    # Fetch all the rows and convert them to a list of dictionaries
-    rows = cur.fetchall()
-
-    data = [{'user_id': row[0],
-             'name': row[1],
-             'review_count': row[2],
-             'yelping_since': row[3],
-             'friends': row[4],
-             'average_stars': row[5]
-             } for row in rows]
-
-    # Close the cursor
-    cur.close()
-
-    # Return the data as a JSON response
-    return {'data': data}
-
-# Define a route to get a single row from the database table by id
-
-
-@app.get('/api/user/{id}')
-async def get_user_by_id(id: str):
-    # Create a cursor object
-    cur = conn.cursor()
-
-    # Execute a SELECT statement to get the row with the specified id
-    cur.execute('SELECT * FROM user WHERE user_id = ?', (id,))
-
-    # Fetch the row and convert it to a dictionary
-    row = cur.fetchone()
-    data = {'user_id': row[0],
-             'name': row[1],
-             'review_count': row[2],
-             'yelping_since': row[3],
-             'friends': row[4],
-             'average_stars': row[5]
-            }
-    # Close the cursor
-    cur.close()
-    # Return the data as a JSON response
-    return {'data': data}
-
-
-@app.post("/api/user")
-async def create_user(username: str = Body(...), gender: str = Body(...), age: str = Body(...), country: str = Body(...), password: str = Body(...)):
-    print("Creating user...")
-    newPassword = password
-    password_hash = hashlib.sha256(
-        bytes(newPassword, encoding='utf-8')).hexdigest()
-
-    # Create a cursor object
-    cur = conn.cursor()
-
-    cur.execute('INSERT INTO user (user_id, gender, age, country, registered, password_hash) VALUES (?, ?, ?, ?, ?, ?)',
-                (username, gender, age, country, datetime.now(), password_hash))
-    conn.commit()
-    cur.close()
-    return {"message": "User created successfully!"}
-
-
-@app.post("/api/user/login")
-async def login(username: str = Body(...), password: str = Body(...)):
-    data = await get_user_by_id(username)
-    data = data["data"]
-    newPassword = password
-    password_hash = hashlib.sha256(
-        bytes(newPassword, encoding='utf-8')).hexdigest()
-    if (password_hash == data["password_hash"]):     
-        return {"message": "LogIn successfully!"}
-    return {"message": "Password or account invalid"}
-
 
 """------------------"""
 """ENDPOINTS - REVIEW"""
@@ -127,109 +47,37 @@ async def login(username: str = Body(...), password: str = Body(...)):
 
 @app.get('/api/review')
 async def get_review():
-    # Create a cursor object
-    cur = conn.cursor()
-
-    # Execute a SELECT statement to get all the rows from the table
-    cur.execute('SELECT * FROM review')
-
-    # Fetch all the rows and convert them to a list of dictionaries
-    rows = cur.fetchall()
-    data = [{'review_id': row[0],
-             'user_id': row[1],
-             'business_id': row[2],
-             'stars': row[3],
-             'text': row[4],
-             'date': row[5]
-             } for row in rows]
-
-    # Close the cursor
-    cur.close()
-
-    # Return the data as a JSON response
+    data = df_reviews.to_dict(orient='records')
     return {'data': data}
 
-"""--------------------"""
-"""ENDPOINTS - BUSINESS"""
-"""--------------------"""
+"""------------------"""
+"""ENDPOINTS - MOVIES"""
+"""------------------"""
 
-@app.get('/api/business')
-async def get_business():
-    # Create a cursor object
-    cur = conn.cursor()
-
-    # Execute a SELECT statement to get all the rows from the table
-    cur.execute('SELECT * FROM business')
-
-    # Fetch all the rows and convert them to a list of dictionaries
-    rows = cur.fetchall()
-    data = [{'business_id': row[0],
-             'name': row[1],
-             'address': row[2],
-             'city': row[3],
-             'stars': row[4],
-             'review_count': row[5],
-             'categories': row[6]
-             } for row in rows]
-
-    # Close the cursor
-    cur.close()
-
-    # Return the data as a JSON response
+@app.get('/api/movie')
+async def get_movie():
+    data = df_movies.to_dict(orient='records')
     return {'data': data}
 
-@app.get('/api/business/{id}')
-async def get_business_by_id(id: str):
-    # Create a cursor object
-    cur = conn.cursor()
-
-    # Execute a SELECT statement to get the row with the specified id
-    cur.execute('SELECT * FROM business WHERE business_id = ?', (id,))
-
-    # Fetch the row and convert it to a dictionary
-    row = cur.fetchone()
-    data = {'business_id': row[0],
-             'name': row[1],
-             'address': row[2],
-             'city': row[3],
-             'stars': row[4],
-             'review_count': row[5],
-             'categories': row[6]
-            }
-    # Close the cursor
-    cur.close()
-    # Return the data as a JSON response
+@app.get('/api/movie/{id}')
+async def get_movie_by_id(id: str):  
+    df_movies_id = df_movies.loc[df_movies['movieId'] == int(id)]
+    data = df_movies_id.to_dict(orient='records')
     return {'data': data}
-
 
 # Friends DataFrame
-@app.get('/api/business/recommendation/{user_id}/{top_business}')
-async def get_recommendations(user_id: str, top_business: int):
-    df_business = await get_business()
-    df_business = df_business["data"]
-    df_business = pd.DataFrame(df_business)
-
-    df_review = await get_review()
-    df_review = df_review["data"]
-    df_review = pd.DataFrame(df_review)
-    df = df_review[['user_id', 'business_id', 'stars']]
-
-    svd = dump.load('data/models/svd.pkl')[1]
-    knn = dump.load('data/models/knn.pkl')[1]
-
-    df_user = await get_user()
-    df_user = df_user["data"]
-    df_user = pd.DataFrame(df_user)
-
-    df_user_friends = df_user.copy()
-    df_user_friends['friends'] = df_user_friends['friends'].str.split(', ')
-
-    if len(df_review)>0:    
+@app.get('/api/movie/recommendation/{user_id}/{movie_id}/{n}')
+async def get_recommendations(user_id: int, movie_id: int, n: int):
+    start_time = time.time()
+    if len(df_reviews)>0:    
         print("Waiting for predictions...")
-        prediction = await model.get_top_n(svd, knn, df, df_user_friends, df_business, df_review, user_id, top_business)
-        prediction = prediction.to_dict(orient='records')
+
+        top_n_movies = await model.get_top_n(user_id, movie_id, n, df_movies, df_reviews, df_links, knn)
+        prediction = top_n_movies.to_dict(orient='records')
+        print("--- %s seconds ---" % (time.time() - start_time))
         return {'data': prediction}    
     else:
+        print("--- %s seconds ---" % (time.time() - start_time))
         return {'data': []}
 
 """------"""
--- src/backend/recommendation_systems/model.py
+++ src/backend/recommendation_systems/model.py
@@ -1,164 +1,271 @@
-# Libraries
-seed = 2023
-import random
+
+
 import pandas as pd
 import numpy as np
+import re
+from SPARQLWrapper import SPARQLWrapper, JSON
+import ast
+import random
+seed = 2023
+random.seed(seed)
 
-from surprise import KNNWithZScore, Dataset, Reader
-from surprise.model_selection import train_test_split
+import tmdbsimple as tmdb
+tmdb.API_KEY = 'e41c8a68517883ed8a2d66aca50b3371'
+tmdb.REQUESTS_TIMEOUT = 5  # seconds, for both connect and read
 
-from surprise import Dataset, Reader, KNNWithZScore
-from surprise.model_selection import train_test_split
+# Libraries
+seed = 2023
 
+def preprocess_movie_name(movie_name):
+    # Remove parentheses and their contents using regular expressions
+    processed_name = re.sub(r'\([^()]*\)', '', movie_name)
+    
+    # Remove leading/trailing whitespace
+    processed_name = processed_name.strip()
+    processed_name = processed_name.replace(' ','_')
+    
+    return processed_name
 
-def get_business_city(df_business, df_review_f, business_id, should_print = False):
+def replace_film(movies_list):
+    movies = []
+    remove = [" film", " (film)"]
+    for movie in movies_list:  
+        for i in remove:            
+            movie = movie.replace(i,"").strip()
+        movies.append(movie)    
+    return movies
+
+def get_movie_names_from_dbpedia(movie_uris):
+    # Create a SPARQLWrapper instance and set the DBpedia endpoint URL
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    movies_names = []
+    
+    for i in range(0, len(movie_uris), 5):
+        movies = movie_uris[i:i+5]
+        # Compose the SPARQL query to retrieve the movie names
+        query = """
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+        SELECT ?movieName
+        WHERE {
+          VALUES ?movie {
+            %s
+          }
+          ?movie rdfs:label ?movieName .
+          FILTER (lang(?movieName) = 'en')
+        }
+        """ % ' '.join(['<'+uri+'>' for uri in movies])
+
+        # Set the SPARQL query and request JSON results
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+
+        # Execute the SPARQL query and retrieve the results
+        results = sparql.query().convert()
+
+        # Extract the movie names from the results
+        movie_names_extract = [result['movieName']['value'] for result in results['results']['bindings']]
+        movies_names.extend(movie_names_extract)
+    return movies_names  
+
+def get_movie_names_from_dbpedia(movie_uris):
+    # Create a SPARQLWrapper instance and set the DBpedia endpoint URL
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+   
+    # Compose the SPARQL query to retrieve the movie names
+    query = """
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+
+    SELECT ?movieName
+    WHERE {
+      VALUES ?movie {
+        %s
+      }
+      ?movie rdfs:label ?movieName .
+      FILTER (lang(?movieName) = 'en')
+    }
+    """ % ' '.join(['<'+uri+'>' for uri in movie_uris])
+
+    # Set the SPARQL query and request JSON results
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    # Execute the SPARQL query and retrieve the results
+    results = sparql.query().convert()
+
+    # Extract the movie names from the results
+    movies_names = [result['movieName']['value'] for result in results['results']['bindings']]
+    return movies_names  
+
+def get_similar_movies_by_starring(movie_title, actors_names):
+    # Create a SPARQLWrapper instance and set the DBpedia endpoint URL
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    
+    actors_names = [string.replace(" ", "_") for string in actors_names]
+  
+    # Compose the SPARQL query to retrieve similar movies based on shared actors
+    query = """
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX dbr: <http://dbpedia.org/resource/>
+
+    SELECT DISTINCT ?similarMovie
+    WHERE {
+      ?similarMovie dbo:starring ?actor .
+      FILTER (?actor IN (%s))
+      FILTER (?similarMovie != dbr:%s)
+    }
+    ORDER BY RAND()
+    LIMIT 100
+    """ % (", ".join(["dbr:" + actor_name for actor_name in actors_names]), movie_title)
+    
+    # Set the SPARQL query and request JSON results
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    # Execute the SPARQL query and retrieve the results
+    results = sparql.query().convert()
+
+    # Extract the similar movie URIs from the results
+    similar_movies = [result['similarMovie']['value'] for result in results['results']['bindings']]
+    similar_movies = get_movie_names_from_dbpedia(similar_movies)
+    return similar_movies
+
+def get_similar_movies_by_director(movie_title, director_name):
+    # Create a SPARQLWrapper instance and set the DBpedia endpoint URL
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    
+    director_name = director_name.strip().replace(" ", "_")
+     
+    # Compose the SPARQL query to retrieve similar movies based on shared actors
+    query = """
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    PREFIX dbr: <http://dbpedia.org/resource/>
+
+    SELECT DISTINCT ?similarMovie
+    WHERE {
+      ?similarMovie dbo:director ?director .
+      FILTER (?director = dbr:%s)
+      FILTER (?similarMovie != dbr:%s)
+    }
+    ORDER BY RAND()
+    LIMIT 50
+    """ % (director_name, movie_title)
+
+    # Set the SPARQL query and request JSON results
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+
+    # Execute the SPARQL query and retrieve the results
+    results = sparql.query().convert()
+
+    # Extract the similar movie URIs from the results
+    similar_movies = [result['similarMovie']['value'] for result in results['results']['bindings']]
+    similar_movies = get_movie_names_from_dbpedia(similar_movies)
+    return similar_movies
+
+# Iterate over the DataFrame and call the get_similar_movies function for each movie
+def get_ontological_data(movie_title, director_name, actors_names):
+    similar_movies_starring = get_similar_movies_by_starring(movie_title, actors_names)
+    similar_movies_actors = get_similar_movies_by_director(movie_title, director_name)    
+    similar_movies_starring.extend(similar_movies_actors)
+    
+    similar_movies = replace_film(list(set(similar_movies_starring)))
+    
+    # Create a DataFrame with a single column
+    df = pd.DataFrame({'similar_movies': similar_movies})
+    
+    return df
+
+def get_tmbd_id(movie):
     try:
-        city = df_business[df_business["business_id"] == business_id]["city"].tolist()[0]
-        business_in_city = df_business[df_business["city"] == city]["business_id"].tolist()
-        df_city = df_review_f[df_review_f['business_id'].isin(business_in_city)]
-        df_city = df_city[["user_id","business_id", "stars"]]
-        
-        if should_print:
-            print(city)
-            print("Num. reviews", df_city.shape[0])
-            print("-------------------")
-        return df_city
+        search = tmdb.Search()
+        response = search.movie(query=movie)
+        response_id = response["results"][0]['id']
+        return response_id
     except:
-        return pd.DataFrame()
-
-def get_knn_city(df_business, df_review_f, user_id, business_id, should_print = False):
-    df_city = get_business_city(df_business, df_review_f, business_id, should_print)
+        return np.nan    
     
-    try:
-        # Load data 
-        if df_city.shape[0] >= 5:
-            reader = Reader(rating_scale=(1, 5))
-            data = Dataset.load_from_df(df_city, reader)
-
-            # Split data
-            trainset, testset = train_test_split(data, test_size=0.2, random_state=seed)
-
-            knn = KNNWithZScore(k = 2, sim_options={'name': 'pearson', 'user_based':True}, random_state=seed, verbose=False)
-            knn.fit(trainset)
-            
-            return knn
-        else: 
-            return None
-    except:
-        return None
+def join_ontological_data(df_ontological, df_m_extended, df_l):    
+    df_ontological['tmdbId'] = np.nan
+    df_ontological['tmdbId'] = df_ontological['similar_movies'].apply(lambda x: get_tmbd_id(x))
+    df_ontological['tmdbId'] = df_ontological['tmdbId'].fillna(0).astype(int)
     
-def get_user_friends(df, df_user_friends, user_id, should_print = False):
-    friends_set = df_user_friends[df_user_friends['user_id'] == user_id]
-    if friends_set.shape[0] == 0:
-        return pd.DataFrame()
-    else:
-        friends_set = friends_set["friends"].tolist()[0]
-        friends_set = set(friends_set)
+    columns = ["tmdbId", "movieId", "title", "actors", "director", "keyWords", "similarMovies", "watchProviders"]
 
-        df_friends = df.copy()
-        df_friends = df_friends[df_friends['user_id'].isin(friends_set)]
-        
-        if should_print:
-            print("Friends information")
-            print(df_friends.describe())
-            print("-------------------")
-        return df_friends
-
-def get_knn_friends(df, df_user_friends, user_id, business_id, should_print = False):
-    df_friends = get_user_friends(df, df_user_friends, user_id, should_print)  
+    df_merged = df_ontological.merge(df_l[['tmdbId', 'movieId']], on='tmdbId', how='left')
+    df_merged = df_merged.dropna()
+    df_merged = df_merged.merge(df_m_extended, on='movieId', how='left')
+    df_merged = df_merged[columns]
+    df_merged = df_merged.loc[df_merged['actors'].isna() == False]
+    df_merged['movieId'] = df_merged['movieId'].astype(int)
+    return df_merged
     
-    try:
-        # Load data 
-        if df_friends.shape[0] >= 5:
-            reader = Reader(rating_scale=(1, 5))
-            data = Dataset.load_from_df(df_friends, reader)
+def knn_predict(user_id, item_id, knn):
+    knn_prediction = knn.predict(user_id, item_id).est
+    return knn_prediction
 
-            # Split data
-            trainset, testset = train_test_split(data, test_size=0.2, random_state=seed)
-
-            knn = KNNWithZScore(k = 2, sim_options={'name': 'pearson', 'user_based':True}, random_state=seed, verbose=False)
-            knn.fit(trainset)
-            
-            return knn
-        else: 
-            return None
-    except:
-        return None
-
-def get_weights(city_successful, friends_successful):
-    # Order: SVD, KNN, KNN_City, KNN_Friends
-    # 4 Cases according to the booleans
+async def get_top_n(user_id, movie_id, n, df_m_extended, df, df_l, knn, sample = 1):   
+    movie = df_m_extended.loc[df_m_extended['movieId'] == movie_id] # Movie that the users watches
+    title = preprocess_movie_name(movie['title'].values[0])
+    director = movie['director'].values[0]
+    actors = [value.strip() for value in movie['actors'].values[0].split(',')]
+       
+    # Ontological filtering
+    df_ontological = get_ontological_data(title, director, actors)
+    df_final = join_ontological_data(df_ontological, df_m_extended, df_l)
     
-    if city_successful and friends_successful:
-        return [0.25, 0.5, 0.2, 0.05]
-    elif city_successful and not friends_successful:
-        return [0.3, 0.5, 0.2, 0.0]
-    elif not city_successful and friends_successful:
-        return [0.3, 0.6, 0.0, 0.1]
-    else:
-        return [0.4, 0.6, 0.0, 0.0]
-
-# Make hybrid predictions
-def hybrid_predict(svd, knn, df, df_user_friends, df_business, df_review_f, user_id, business_id, should_print = False):
-    knn_city = get_knn_city(df_business, df_review_f, user_id, business_id, should_print) 
-    knn_friends = get_knn_friends(df, df_user_friends, user_id, business_id, should_print)
-    city_successful = knn_city is not None
-    friends_successful = knn_friends is not None
-    
-    svd_prediction = svd.predict(user_id, business_id).est
-    knn_prediction = knn.predict(user_id, business_id).est
-    knn_city_prediction = 0
-    knn_friends_prediction = 0
-
-    if city_successful:
-        knn_city_prediction = knn_city.predict(user_id, business_id).est
-
-    if friends_successful:
-        knn_friends_prediction = knn_friends.predict(user_id, business_id).est
-
-    if should_print:
-        print("SVD:", svd_prediction)
-        print("KNN:", knn_prediction)
-        print("KNN City:", "There is not enough information to create a recommendation model." if not city_successful else knn_city_prediction)
-        print("KNN Friends:", "There is not enough information to create a recommendation model." if not friends_successful else knn_friends_prediction)
-        print("-------------------")
-    
-    models_predictions = [svd_prediction, knn_prediction, knn_city_prediction, knn_friends_prediction]
-    weights = get_weights(city_successful, friends_successful)
-    
-    hybrid_prediction = np.dot(weights, models_predictions)
-    
-    return hybrid_prediction
-
-async def get_top_n(svd, knn, df, df_user_friends, df_business, df_review_f, user_id, n):
-    print("User:", user_id)
+    print("Data retrieved:", len(df_final))
+    print()
     
     # Get all businesses the user has not reviewed
-    all_businesses = set(df['business_id'].unique())
-    reviewed_businesses = set(df[df['user_id'] == user_id]['business_id'].unique())
-    unreviewed_businesses = all_businesses - reviewed_businesses
-    print("Total business:", len(unreviewed_businesses))
+    all_items = set(df_final['movieId'].unique())
+    reviewed_items = set(df.loc[df['userId'] == user_id]['movieId'].unique())
+    unreviewed_items = all_items - reviewed_items
     
-    unreviewed_businesses = list(unreviewed_businesses)
-    num_business = len(unreviewed_businesses)
-    num_sample = int(num_business * 0.01)
-    unreviewed_businesses = random.sample(unreviewed_businesses, num_sample)
+    print("Ontological filtered movies:", all_items)
+    print()
+    print("Reviewed:",reviewed_items)
+    print()
+    print("Unreviewed:", unreviewed_items)
+    print()
+    print("Total movies unreviewed:", len(unreviewed_items))
+    print()
+   
+    unreviewed_items = list(unreviewed_items)
+    num_items = len(unreviewed_items)
+    num_sample = int(num_items * sample)
+    unreviewed_items = random.sample(unreviewed_items, num_sample)
+    
+    print("Predicting movies...")
     
     # Make predictions for unreviewed businesses and sort by prediction
     predictions = []
-    for business_id in unreviewed_businesses:
-        prediction = hybrid_predict(svd, knn, df, df_user_friends, df_business, df_review_f, user_id, business_id, False)
-        predictions.append((business_id, prediction))
+    for item_id in unreviewed_items:
+        prediction = knn_predict(user_id, item_id, knn)
+        predictions.append((item_id, prediction))
         #print((business_id, prediction))
     predictions.sort(key=lambda x: x[1], reverse=True)
+    
+    print("Retrieving data from DBPedia...")
+    print()
+    
+    print("User:", user_id)
+    print()
+    print("Movie title:", movie['title'].values[0])
+    print("Director:", director)
+    print("Actors:", actors)
+    print()
 
     # Get top n recommended businesses and their predictions
     top_n = predictions[:n]
-    top_n_businesses = [x[0] for x in top_n]
+    top_n_items = [x[0] for x in top_n]
     top_n_predictions = [x[1] for x in top_n]
     top_n_percentage = [str(round(x/5*100, 2))+"%" for x in top_n_predictions]
 
     # Create DataFrame with business ids and predictions
-    df_top_n = pd.DataFrame({'business_id': top_n_businesses, 'prediction': top_n_predictions, 'percentage': top_n_percentage})
-    df_top_n =  pd.merge(df_top_n, df_business, on='business_id')
+    columns = ["tmdbId", "movieId", "title", "actors", "director", "keyWords", "similarMovies", "watchProviders"]
+
+    df_top_n = pd.DataFrame({'movieId': top_n_items, 'prediction': top_n_predictions, 'percentage': top_n_percentage})
+    df_top_n =  pd.merge(df_top_n, df_final, on='movieId')
     return df_top_n
+
